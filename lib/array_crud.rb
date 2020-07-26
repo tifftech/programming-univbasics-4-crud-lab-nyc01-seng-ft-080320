d12522d8337312385a2e3fbd5f1fa5e2c27de382
Tiffani Best <tiffanimelissabest@gmail.com>
2020-07-26 22:45:38 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   array =["pink","grey","green","yellow"]
-  a.push ("blue")
+  array.push ("blue")
 end
 
 def add_element_to_start_of_array(array, element)
