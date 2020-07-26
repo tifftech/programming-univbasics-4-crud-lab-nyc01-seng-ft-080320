b2e5cc6fdf1cc8ef15fb8938a23ba75c4d46bdff
Tiffani Best <tiffanimelissabest@gmail.com>
2020-07-26 22:46:06 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array =["pink","grey","green","yellow"]
-  array.push ("blue")
+  
 end
 
 def add_element_to_start_of_array(array, element)
